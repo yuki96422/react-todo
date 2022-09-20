12ba250b2c19215c4af8f3ff35a95be83af07540
yuki96422 <nobuhara422@gmail.com>
2022-09-21 00:35:34 +0900

    chakra ui icon インストール

--- react_todo/src/components/pages/TaskList.jsx
+++ react_todo/src/components/pages/TaskList.jsx
@@ -1,17 +1,21 @@
-import { Flex, Heading, VStack, Box } from '@chakra-ui/react';
+import {Heading, VStack, Box} from '@chakra-ui/react';
+import {CheckIcon} from "@chakra-ui/icons"
+
 
 const TaskList = () => {
   return (
-    <Flex p={10}>
+    <Box p={10}>
       <Heading as="h1" color="gray.600" fontSize={32}>
         Task List
       </Heading>
 
-      <VStack>
-        <Box bg="white">aa</Box>
-        <Box bg="white">aa</Box>
+      <VStack spacing={6} alignItems="start" p={10}>
+   
+        <Box bg="white" w="800px" h="50px" p="10px" borderRadius="10px" lineHeight="25px"><CheckIcon p="5px"/>aa</Box>
+        <Box bg="white" w="800px" h="50px" p="10px" borderRadius="10px" lineHeight="25px">aa</Box>
+        <Box bg="white" w="800px" h="50px" p="10px" borderRadius="10px" lineHeight="25px">aa</Box>
       </VStack>
-    </Flex>
+    </Box>
   );
 };
 
