bced33d2264c683c14a00ff85d0c0c59c7198967
yuki96422 <nobuhara422@gmail.com>
2022-09-22 10:14:48 +0900

    task card作成

--- react_todo/src/components/pages/TaskList.jsx
+++ react_todo/src/components/pages/TaskList.jsx
@@ -1,19 +1,17 @@
 import {Heading, VStack, Box} from '@chakra-ui/react';
-import {CheckIcon} from "@chakra-ui/icons"
+
+import TaskCard from '../taskcard/Taskcard';
 
 
 const TaskList = () => {
   return (
     <Box p={10}>
-      <Heading as="h1" color="gray.600" fontSize={32}>
+      <Heading as="h1" color="gray.600" fontSize="32px">
         Task List
       </Heading>
 
-      <VStack spacing={6} alignItems="start" p={10}>
-   
-        <Box bg="white" w="800px" h="50px" p="10px" borderRadius="10px" lineHeight="25px"><CheckIcon p="5px"/>aa</Box>
-        <Box bg="white" w="800px" h="50px" p="10px" borderRadius="10px" lineHeight="25px">aa</Box>
-        <Box bg="white" w="800px" h="50px" p="10px" borderRadius="10px" lineHeight="25px">aa</Box>
+      <VStack spacing="10px" alignItems="start" p="20px">
+        <TaskCard />
       </VStack>
     </Box>
   );
---
+++ react_todo/src/components/taskcard/Taskcard.jsx
@@ -0,0 +1,25 @@
+import { Box, Flex } from '@chakra-ui/react';
+import { CheckIcon, CloseIcon } from '@chakra-ui/icons';
+
+const TaskCard = () => {
+  return (
+    <Flex alignItems="center">
+      <Box bg="white" w="800px" h="50px" p="10px" borderRadius="10px" lineHeight="30px" shadow="md">
+        <CheckIcon
+          w="20px"
+          h="20px"
+          ml="10px"
+          mr="20px"
+          color="teal.800"
+          cursor="pointer"
+          _hover={{ color: 'teal.300' }}
+          transition="0.2s"
+        />
+        タスクですタスクタスクですタスクです
+      </Box>
+      <CloseIcon  ml="20px"  cursor="pointer"/>
+    </Flex>
+  );
+};
+
+export default TaskCard;
